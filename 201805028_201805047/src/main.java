6cc6d4800502cc77f0792c4760172fb952383194
HsarkutH <sarkut.huseyin@gmail.com>
2022-12-19 14:26:41 +0300

    Update 14:26

--- 201805028_201805047/src/main.java
+++ 201805028_201805047/src/main.java
@@ -1,11 +1,50 @@
 import java.util.Scanner;
 
-public class main {
+public class Items{
+	private int quantity;
+	private int barcodeNumber;
+	private String productName;
+	Items(int quantity, int barcodeNumber,String productName){
+		this.quantity = quantity;
+		this.barcodeNumber = barcodeNumber;
+		this.productName = productName;
+		if(quantity<0)
+		{
+			System.out.println("This is not possible. Quantity can't be negative.");
+			System.exit(0);
+		}
+		if(barcodeNumber<0)
+		{
+			System.out.println("This is not possible. Barcode can't be negative.");
+			System.exit(0);
+		}
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public int getBarcodeNumber() {
+		return barcodeNumber;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	
+	public void setQuantity(int quantity){
+		this.quantity = quantity;
+	}
+	public void setBarcodeNumber(int barcodeNumber) {
+		this.barcodeNumber = barcodeNumber;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+}
+
+public class Main {
 
 	public static void main(String[] args) {
-		String productName;
-		int quantity;
-		int barcodeNumber;
+		
 		int choice;
 		
 		
