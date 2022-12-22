f9496dd3fbe4349a5c4dd18ee1f5760a2477803f
HsarkutH <sarkut.huseyin@gmail.com>
2022-12-22 17:33:57 +0300

    update 22.12 17:33

--- 201805028_201805047/Assignment_2/src/Main.java
+++ 201805028_201805047/Assignment_2/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.LinkedList;
 
 
 
@@ -8,17 +9,40 @@ public class Main {
 		
 		int choice;
 		int i = 0;
+		//int arrSize=5;
+		int itemchoose;
+		int increaseAmount;
+		String newItem;
+		int newQuantity;
+		int newBarcodeNumber;
 			
 			
-			
+		LinkedList<String> productNames = new LinkedList<>();
+		LinkedList<Integer> quantity = new LinkedList<>();
+		LinkedList<Integer> barcodeNumber = new LinkedList<>();
 		Scanner scan = new Scanner(System.in);
-		Items[] item = new Items[10];
-		item[0] = new Items(10,20,"Ekler");
+		
+		productNames.addFirst("Ekler");
+		productNames.add("Baklava");
+		productNames.add("Pasta");
+		productNames.add("Bomba");
+		productNames.add("Kurabiye");
+		quantity.add(20);
+		quantity.add(10);
+		quantity.add(5);
+		quantity.add(30);
+		quantity.add(50);
+		barcodeNumber.add(42);
+		barcodeNumber.add(86);
+		barcodeNumber.add(95);
+		barcodeNumber.add(13);
+		barcodeNumber.add(79);
+		/*item[0] = new Items(50,20,"Ekler");
 		item[1] = new Items(100,15,"Baklava");
-		item[2] = new Items(100,15,"Baklava");
-		item[3] = new Items(100,15,"Baklava");
-		item[4] = new Items(100,15,"Baklava");
-		item[5] = new Items(100,15,"Erik");
+		item[2] = new Items(20,3,"Pasta");
+		item[3] = new Items(150,18,"Bomba");
+		item[4] = new Items(200,42,"Kurabiye");*/
+		
 			
 			//menu
 		while(true) {	
@@ -30,7 +54,7 @@ public class Main {
 			System.out.println("5 - Print post-order");
 			System.out.println("6 - Print pre-order\n");
 			System.out.println("0 - Exit\n");
-			System.out.println("Please choose your operation: ");
+			System.out.print("Please choose your operation: ");
 			choice = scan.nextInt();
 			System.out.println("\n\n-------------------------------------\n\n");
 			
@@ -40,15 +64,59 @@ public class Main {
 				
 				
 				case 1:
-					while(item[i].getProductName().equals("")==false)
+					System.out.print("Please enter the barcode number of new item: ");
+					newBarcodeNumber = scan.nextInt();
+					System.out.print("Please enter the quantityy of new item: ");
+					newQuantity = scan.nextInt();
+					scan.nextLine();
+					System.out.print("Please enter the product name: ");
+					newItem = scan.nextLine();
+					System.out.println();
+					
+					productNames.add(newItem);
+					quantity.add(newQuantity);
+					barcodeNumber.add(newBarcodeNumber);
+					
+					/*while(i<productNames.size())
 					{
-						item[i].toString();
+						System.out.println("Product Name: "+ productNames.get(i));
+						System.out.println("Quantity: "+ quantity.get(i));
+						System.out.println("Barcode Number: "+barcodeNumber.get(i));
+						System.out.println();
 						i++;
 					}
+					
+					System.out.println(barcodeNumber.size());
+					i=0;*/
 					break;
 				case 2:
+					while(i<productNames.size())
+					{
+						System.out.println(i+1 +": "+productNames.get(i));
+						i++;
+					}
+					System.out.print("Please choose the item: ");
+					itemchoose = scan.nextInt()-1;
+					System.out.print("Amount of increase: ");
+					increaseAmount = scan.nextInt();
+					
+					quantity.set(itemchoose, quantity.get(itemchoose)+increaseAmount);
+					//System.out.println(quantity.get(itemchoose));
+					i=0;
 					break;
 				case 3:
+					while(i<productNames.size())
+					{
+						System.out.println(i+1 +": "+productNames.get(i));
+						i++;
+					}
+					System.out.print("Please choose the item: ");
+					itemchoose = scan.nextInt()-1;
+					System.out.print("Amount of decrease: ");
+					increaseAmount = scan.nextInt();
+					
+					quantity.set(itemchoose, quantity.get(itemchoose)-increaseAmount);
+					i=0;
 					break;
 				case 4:
 					break;
